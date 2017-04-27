aecd23edbde2d1f761f5e52401c1b544002350ec
jcpasion <jcpasion@ucdavis.edu>
2017-04-27 10:40:57 -0700

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,9 +1,11 @@
+#function returns the reverse of a string
 def reverse(s):
     rev= ""
     for ch in s:
         rev = ch+rev
     return (rev)
 
+#function determines if string is a palindrome
 def is_palindrome1(s):
     rev = reverse(s)
     return rev == s
